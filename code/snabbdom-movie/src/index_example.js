4dc641f8f1d16dece0eae65fe1580ff759f9b5a5
Valen <wang_wenl@worksap.co.jp>
2020-07-22 12:54:57 +0800

    init index.js from example code with only movie data

--- code/snabbdom-movie/src/index_example.js
+++ code/snabbdom-movie/src/index_example.js
@@ -97,3 +97,119 @@ var data = [
   originalData[8],
   originalData[9],
 ];
+
+function changeSort(prop) {
+  sortBy = prop;
+  data.sort((a, b) => {
+    if (a[prop] > b[prop]) {
+      return 1;
+    }
+    if (a[prop] < b[prop]) {
+      return -1;
+    }
+    return 0;
+  });
+  render();
+}
+
+function add() {
+  var n = originalData[Math.floor(Math.random() * 10)];
+  data = [
+    { rank: nextKey++, title: n.title, desc: n.desc, elmHeight: 0 },
+  ].concat(data);
+  render();
+  render();
+}
+
+function remove(movie) {
+  data = data.filter((m) => {
+    return m !== movie;
+  });
+  render();
+}
+
+function movieView(movie) {
+  return h(
+    "div.row",
+    {
+      key: movie.rank,
+      style: {
+        opacity: "0",
+        transform: "translate(-200px)",
+        delayed: { transform: `translateY(${movie.offset}px)`, opacity: "1" },
+        remove: {
+          opacity: "0",
+          transform: `translateY(${movie.offset}px) translateX(200px)`,
+        },
+      },
+      hook: {
+        insert: (vnode) => {
+          movie.elmHeight = vnode.elm.offsetHeight;
+        },
+      },
+    },
+    [
+      h("div", { style: { fontWeight: "bold" } }, movie.rank),
+      h("div", movie.title),
+      h("div", movie.desc),
+      h("div.btn.rm-btn", { on: { click: [remove, movie] } }, "x"),
+    ]
+  );
+}
+
+function render() {
+  data = data.reduce((acc, m) => {
+    var last = acc[acc.length - 1];
+    m.offset = last ? last.offset + last.elmHeight + margin : margin;
+    return acc.concat(m);
+  }, []);
+  totalHeight = data[data.length - 1].offset + data[data.length - 1].elmHeight;
+  vnode = patch(vnode, view(data));
+}
+
+function view(data) {
+  return h("div#container", [
+    h("h1", "Top 10 movies"),
+    h("div", [
+      h("a.btn.add", { on: { click: add } }, "Add"),
+      "Sort by: ",
+      h("span.btn-group", [
+        h(
+          "a.btn.rank",
+          {
+            class: { active: sortBy === "rank" },
+            on: { click: [changeSort, "rank"] },
+          },
+          "Rank"
+        ),
+        h(
+          "a.btn.title",
+          {
+            class: { active: sortBy === "title" },
+            on: { click: [changeSort, "title"] },
+          },
+          "Title"
+        ),
+        h(
+          "a.btn.desc",
+          {
+            class: { active: sortBy === "desc" },
+            on: { click: [changeSort, "desc"] },
+          },
+          "Description"
+        ),
+      ]),
+    ]),
+    h(
+      "div.list",
+      { style: { height: totalHeight + "px" } },
+      data.map(movieView)
+    ),
+  ]);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  var container = document.getElementById("container");
+  vnode = patch(container, view(data));
+  render();
+});
